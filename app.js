6d4ebab0d05fe27239b031d0786889aefb27d72c
Bryan Green <bryan@Bryans-MacBook-Pro.local>
2014-09-27 22:07:41 -0700

    initial commit of completed app

---
+++ app.js
@@ -0,0 +1,61 @@
+/*globals require, process*/
+
+var _ = require('lodash');
+var fs = require('fs');
+var readline = require('readline');
+
+var rd = readline.createInterface({
+    input: fs.createReadStream('./resales.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+var stream = fs.createWriteStream("./mergedSales.txt");
+var sum = function (numbers) {
+    return _.reduce(numbers, function (memo, num) {
+        return memo + num;
+    }, 0);
+};
+
+var fields = ['city', 'sfh sales', 'median price', 'price change', 'condo sales', 'condo median',
+    'condo change', 'sfr median price per sft'];
+stream.once('open', function () {
+    var rawData = {};
+    var fieldNames = fields.join('\t');
+    stream.write(fieldNames + '\n');
+    console.log(fieldNames);
+    rd.on('line', function (line) {
+        var input = line.replace(/[%\$,]+/gi, '').split('\t');
+        var city = input.splice(0, 1);
+        input.splice(0, 1);
+        var numData = _.map(input, function (string) {
+            return parseInt(string);
+        });
+        rawData[city] = rawData[city] || [];
+        rawData[city].push(numData);
+    });
+
+    rd.on('close', function () {
+        var cityData = {};
+        _.each(_.keys(rawData), function (city) {
+            var transposed = _.zip(rawData[city]);
+            cityData[city] = _.map(transposed, function (fieldArray) {
+                fieldArray = _.reject(fieldArray, function (value) {
+                    return isNaN(value);
+                });
+                return sum(fieldArray) / fieldArray.length;
+            });
+        });
+
+        _.each(_.keys(cityData), function (city) {
+            var prettier = _.map(cityData[city], function (field) {
+                return isNaN(field) ? 'n/a' : Math.round(field);
+            });
+            var cityAverages = prettier.join('\t');
+            var output = city + '\t' + cityAverages;
+            stream.write(output + '\n');
+            console.log(output);
+        });
+        stream.end();
+    });
+});
